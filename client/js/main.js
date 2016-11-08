5709dc972619b02bd320e731e38171d8ff7a07f1
Austin R. Snider <deserteagle.pwn@gmail.com>
2016-11-08 01:47:00 -0500

    Started .js

---
+++ client/js/main.js
@@ -0,0 +1,12 @@
+function onload()
+{
+	var charNameBox = document.getElementById("CharacterNameBox");
+	
+}
+
+function updateCharName()
+{
+	
+	var charName = charNameBox.value;
+	alert(charName);
+};
--- client/main.js
+++ client/main.js
@@ -1,22 +1,12 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+function onload()
+{
+	var charNameBox = document.getElementById("CharacterNameBox");
+	
+}
 
-import './main.html';
-
-Template.main.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.main.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.main.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+function updateCharName()
+{
+	
+	var charName = charNameBox.value;
+	alert(charName);
+};
